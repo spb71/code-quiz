9f1510ff5887c845c473bc36791730fbb69f5aa6
spb71 <shem.baijoo@gmail.com>
2021-09-01 23:20:27 -0400

    Fixed answer button typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,28 @@ var timerEl = document.querySelector(".countdown");
 var questions = [
     {
         question:"Commonly used data types DO NOT include:",
-        answers:["1. strings","2. booleans","3. alerts","4. numbers"],
-        correctAnswer:"alerts"        
+        answers:["a. strings","b. booleans","c. alerts","d. numbers"],
+        correctAnswer:"c. alerts"        
     },
     {
         question:"The condition in an if/else statement is enclosed within:",
-        answers:["1. quotes","2. curly braces","3. parentheses","4. square brackets"],
-        correctAnswer:"parentheses"
+        answers:["a. quotes","b. curly braces","c. parentheses","d. square brackets"],
+        correctAnswer:"c. parentheses"
     },
     {
         question:"Arrays in Javascript can be used to store ______.",
-        answers:["1. numbers and strings","2. other arrays","3. booleans","4. all of the above"],
-        correctAnswer:"all of the above"
+        answers:["a. numbers and strings","b. other arrays","c. booleans","d. all of the above"],
+        correctAnswer:"d. all of the above"
     },
     {
         question:"String values must be enclosed within ______ when being assigned to variables.",
-        answers:["1. commas","2. curly braces","3. quotes","4. parentheses"],
-        correctAnswer:"quotes"
+        answers:["a. commas","b. curly braces","c. quotes","d. parentheses"],
+        correctAnswer:"c. quotes"
     },
     {
         question:"A very useful tool during development and debugging for printing content to the debugger is:",
-        answers:["1. Javascript","2. terminal/bash","3. for loops","4. console.log"],
-        correctAnswer:"console.log"
+        answers:["a. Javascript","b. terminal/bash","c. for loops","d. console.log"],
+        correctAnswer:"d. console.log"
     }
 
 ];
